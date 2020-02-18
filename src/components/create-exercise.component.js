c71b64731505a9689a476bc301026e5bf25b6f4d
Malik Ebers <malik.ebers@web.de>
2020-02-18 21:57:28 +0100

    added the create exercise function

--- src/components/create-exercise.component.js
+++ src/components/create-exercise.component.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import DatePicker from 'react-datepicker';
+import axios from 'axios';
 import "react-datepicker/dist/react-datepicker.css";
 
 export default class CreateExercise extends Component {
@@ -22,10 +23,15 @@ export default class CreateExercise extends Component {
   }
 
   componentDidMount() {
-    this.setState({
-      users: ['testing'],
-      username: 'test user'
-    });
+    axios.get('http://localhost:5000/exercises/add', exercise)
+    .then(response => {
+      if(response.data.length > 0) {
+        this.setState({
+          users: response.data.map(user => user.username),
+          username: response.data[0].username
+        })
+      }
+    })
   }
 
   onChangeUsername(e) {
@@ -60,6 +66,9 @@ export default class CreateExercise extends Component {
       }
 
       console.log(exercise);
+
+      axios.post('http://localhost:5000/exercise/add', exercise)
+      .tehn(res => console.log(res.data));
 
       window.location = '/';
     }
